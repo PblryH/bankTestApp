76db711222648188cd695499fd5a0e318975652c
Раиль Камалов <kamalov@fix.ru>
2016-04-30 16:30:29 +0300

    init

---
+++ app/src/main/java/pro/rgun/banktestapp/ActivityMain.java
@@ -0,0 +1,13 @@
+package pro.rgun.banktestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
